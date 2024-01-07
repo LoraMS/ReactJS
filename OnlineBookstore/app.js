3cd4d3b102e5f8a420f09b3786e5ed85663465f6
lilivs <81024574+lilivs@users.noreply.github.com>
2024-01-07 03:27:23 +0200

    editing
    editing

--- OnlineBookstore/app.js
+++ OnlineBookstore/app.js
@@ -5,27 +5,31 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
-// const port = process.env.PORT || 3001;
 
 const book = require('./routes/book');
 const event = require('./routes/event');
-
-const app = express();
-
 const auth = require('./routes/auth');
+
+const app = express(); 
+app.use(logger('dev'));
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, 'build')));
+}
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'build'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 'extended':'false' }));
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.PROD_MONGODB || 'mongodb://Admin:admin1@bookstore-shard-00-00.dd7jg.mongodb.net:27017,bookstore-shard-00-01.dd7jg.mongodb.net:27017,bookstore-shard-00-02.dd7jg.mongodb.net:27017/bookstore?ssl=true&replicaSet=atlas-104jno-shard-0&authSource=admin&retryWrites=true&w=majority', {promiseLibrary: require('bluebird') })
-// mongoose.connect('mongodb://Admin:admin1@ds261040.mlab.com:61040/bookstore', {promiseLibrary: require('bluebird') })
-// mongoose.connect('mongodb://localhost:27017/bookstore', {promiseLibrary: require('bluebird')})
-  .then(() => console.log('connection succesful'))
-  .catch((err) => console.error(err));
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ 'extended':'false' }));
-app.use(express.static(path.join(__dirname, 'build')));
+ mongoose.connect(process.env.MONGODB_URI || 'mongodb://Admin:admin1@bookstore-shard-00-00.dd7jg.mongodb.net:27017,bookstore-shard-00-01.dd7jg.mongodb.net:27017,bookstore-shard-00-02.dd7jg.mongodb.net:27017/bookstore?ssl=true&replicaSet=atlas-104jno-shard-0&authSource=admin&retryWrites=true&w=majority', {promiseLibrary: require('bluebird') })
+  .then(() => console.log('Connection succesful!'))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(fileUpload());
@@ -55,5 +59,11 @@ app.use(function(err, req, res, next) {
 });
 
 // app.listen(port, () => console.log(`Server listening on port ${port}`));
+// const server = createServer(app);
+// server.listen(port, err => {
+//   if (err) throw err;
+
+//   console.log('Server started!');
+// })
 
 module.exports = app;
--- OnlineBookstore/src/components/home/HomePage.jsx
+++ OnlineBookstore/src/components/home/HomePage.jsx
@@ -16,7 +16,7 @@ const Home = () => {
             <div className="col-lg-4 text-center">
               <img className="rounded-circle" src={require('./store.jpg')} width="140" height="140" alt="prod1" />
               <h2>Books</h2>
-              <p className="text-justify">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+              <p className="text-justify">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
               <p><Link to="/catalog" className="btn btn-secondary">View books »</Link></p>
             </div>
             <div className="col-lg-4 text-center">
